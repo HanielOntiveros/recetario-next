852cc7b1086ce68da36cb10d30fda835fdf3eb62
lexardi <lexardi@MacBook-Pro-de-lexardi.local>
2023-01-17 07:51:05 +0100

    add images to recipes

--- pages/recipe.jsx
+++ pages/recipe.jsx
@@ -1,25 +1,24 @@
-import client from "@/lib/client";
+import { client } from "@/lib/client";
 import Recipes from "@/components/Recipes";
 
 export default function Recipe({ recipes }) {
   return (
     <>
       <div className="flex gap-4">
-            {recipes.map((recipe) => (
-              <Recipes key={recipe._id} {...recipe} />
-            ))}
-          </div>
+        {recipes.map((recipe) => (
+          <Recipes key={recipe._id} {...recipe} />
+        ))}
+      </div>
     </>
   );
 }
 
 export async function getStaticProps() {
-  const recipes = await client.fetch(`*[_type == "recipes"]`);
+  const recipes = await client.fetch(`*[_type == "recipe"]`);
 
   return {
     props: {
-      recipes
-    }
+      recipes,
+    },
   };
 }
-
--- pages/recipe/[slug].js
+++ pages/recipe/[slug].js
@@ -1,17 +1,14 @@
 import Head from "next/head";
 import { urlFor } from "../../lib/client";
 import { client } from "../../lib/client";
-import { format } from "date-fns";
 import Image from "next/image";
-import Content from "@/components/Content";
-export default function Post({ post }) {
-  const date = format(new Date(post.publishedDate), "dd MMM yyy");
+export default function Post({ recipe }) {
   return (
     <>
       <Head></Head>
       <div className="relative flex w-full h-screen">
         <Image
-          src={urlFor(post.image).url()}
+          src={urlFor(recipe.image).url()}
           alt=""
           width="1920"
           height="100"
@@ -22,17 +19,14 @@ export default function Post({ post }) {
         <div className="container mx-auto ">
           <div className="items-center justify-center ">
             <div className="flex items-center justify-center">
-              <p className="pr-6 text-sm font-semibold text-center text-white uppercase font-mont">
-                {date}
-              </p>
               <p className="text-white">|</p>
               <p className="pl-6 text-lg font-light text-center text-white uppercase font-mont">
-                {post.lekua}
+                {recipe.lekua}
               </p>
             </div>
             <div className="py-2">
               <p className="text-5xl font-semibold text-white font-mont">
-                {post.title}
+                {recipe.title}
               </p>
             </div>
           </div>
@@ -40,24 +34,22 @@ export default function Post({ post }) {
       </div>
 
       <div className="container flex py-4 mx-auto bg-blue-200">
-        <div className="">
-          <Content body={post.body} />
-        </div>
+        <div className=""></div>
       </div>
     </>
   );
 }
 export async function getStaticPaths() {
-  const query = `*[_type == "post"] {
+  const query = `*[_type == "recipe"] {
         slug {
             current
         }
     }`;
 
-  const posts = await client.fetch(query);
-  const paths = posts.map((post) => ({
+  const recipes = await client.fetch(query);
+  const paths = recipes.map((recipe) => ({
     params: {
-      slug: post.slug.current,
+      slug: recipe.slug.current,
     },
   }));
 
@@ -68,13 +60,13 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params: { slug } }) {
-  const query = `*[_type == "post" && slug.current == '${slug}'][0]`;
+  const query = `*[_type == "recipe" && slug.current == '${slug}'][0]`;
 
-  const post = await client.fetch(query);
+  const recipe = await client.fetch(query);
 
   return {
     props: {
-      post,
+      recipe,
     },
   };
 }
